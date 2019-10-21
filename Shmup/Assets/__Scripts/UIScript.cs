f24932644383d4d5c81083c24ef169162115b865
Ethan Choe <dongjoon.choe@colorado.edu>
2019-10-21 11:23:39 -0600

    Began UI

---
+++ Shmup/Assets/__Scripts/UIScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour
+{
+    public Text Score;
+    public Text Lives;
+    private int scoreCnt, livesCnt;
+    public static UIScript S;
+    void Awake()
+    {
+        scoreCnt = 0;
+        livesCnt = 1;
+        S = this;
+    }
+
+    // Update is called once per frame
+    public void UpdateScore()
+    {
+        Score.text = scoreCnt.ToString();
+
+        Lives.text = livesCnt.ToString();
+    }
+}
